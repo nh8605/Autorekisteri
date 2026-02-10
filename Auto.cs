9c6ea8f08284da37ccea0c0089cd1ac191cc8674
nh8605 <niklashietakangas@gmail.com>
2026-02-10 10:29:18 +0200

    Add files via upload

---
+++ Auto.cs
@@ -0,0 +1,21 @@
+﻿namespace OmaOlioProjekti
+{
+	class Auto
+	{
+		public string Rekisterinumero { get; }
+		public string Merkki { get; }
+		public string Malli { get; }
+
+		public Auto(string rekisterinumero, string merkki, string malli)
+		{
+			Rekisterinumero = rekisterinumero;
+			Merkki = merkki;
+			Malli = malli;
+		}
+
+		public override string ToString()
+		{
+			return $"{Rekisterinumero};{Merkki};{Malli}";
+		}
+	}
+}
---
+++ Autorekisteri.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OmaOlioProjekti
+{
+	class Autorekisteri
+	{
+		private List<Auto> autot = new List<Auto>();
+
+		public void LisaaAuto(Auto auto)
+		{
+			autot.Add(auto);
+		}
+
+		public Auto HaeAuto(string rekisterinumero)
+		{
+			foreach (var auto in autot)
+			{
+				if (auto.Rekisterinumero.Equals(rekisterinumero, StringComparison.OrdinalIgnoreCase))
+					return auto;
+			}
+			return null;
+		}
+
+		public bool PoistaAuto(string rekisterinumero)
+		{
+			Auto poistettava = HaeAuto(rekisterinumero);
+
+			if (poistettava != null)
+			{
+				autot.Remove(poistettava);
+				return true;
+			}
+			return false;
+		}
+
+		public void ListaaAutot()
+		{
+			Console.WriteLine("\nRekisterissä olevat autot:");
+			foreach (var auto in autot)
+				Console.WriteLine($"{auto.Rekisterinumero} – {auto.Merkki} {auto.Malli}");
+		}
+
+		public void TallennaTiedostoon(string polku)
+		{
+			using (StreamWriter sw = new StreamWriter(polku))
+			{
+				foreach (var auto in autot)
+					sw.WriteLine(auto.ToString());
+			}
+		}
+
+		public void LataaTiedostosta(string polku)
+		{
+			if (!File.Exists(polku))
+				return;
+
+			autot.Clear();
+
+			foreach (var rivi in File.ReadAllLines(polku))
+			{
+				string[] osat = rivi.Split(';');
+				if (osat.Length == 3)
+					autot.Add(new Auto(osat[0], osat[1], osat[2]));
+			}
+		}
+	}
+}
+
